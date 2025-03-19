169dfa038b3a8c0980e383365e2552320da396bc
wendellu18 <161165357+wendellu18@users.noreply.github.com>
2025-03-19 15:11:03 -0300

    Add files via upload

---
+++ atividade-01/ex02.py
@@ -0,0 +1,6 @@
+segundos = int(input("informe a quantidade de segundos: "))
+minutos = segundos / 60
+horas = minutos / 60
+dias = horas / 24
+
+print(segundos, "segundos equivalem a:", minutos, "minutos, que equivalem a:", horas, "horas que equivalem a:", dias, "dias" )
