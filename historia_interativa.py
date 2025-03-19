ecb1308a3261a8b784cb8789e957e9555c8154b1
wendellu18 <161165357+wendellu18@users.noreply.github.com>
2025-03-19 16:51:41 -0300

    Add files via upload

---
+++ historia_interativa.py
@@ -0,0 +1,8 @@
+heroi = input("digite o nome de seu herói: ")
+mestre = input("digite o nome do mestre de seu herói: ")
+vilao = input("digite o nome de vilão: ")
+arma_heroi = input("informe a arma utilizada pelo seu heroi: ")
+bioma = input("digite o nome do bioma que a historia se passa: ")
+aparicao_supresa = input("digite o nome de um aliado supresa: ")
+print()
+print(f"Após o vilão {vilao} matar seu mestre {mestre}, o herói {heroi} sai em busca de vingança. Com sua poderosa {arma_heroi} {heroi} atravessa a dureza do bioma de {bioma} atras do esconderijo de {vilao}, ao chegar no esconderijo {heroi} desafia {vilao} para uma luta mortal. Ao decorrer da luta {vilao} se mostra superior a {heroi} ate que {aparicao_supresa} chega e ajuda {heroi} a vingar seu mestre {mestre}")
