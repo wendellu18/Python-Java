02321994893d87ff16094d989f930a6d8b859e37
umjotta <heliojuniorsly@gmail.com>
2025-03-29 14:08:19 -0300

    HELIO-Wendell_history.py

---
+++ HELIO-Wendell_history.py
@@ -0,0 +1,16 @@
+heroi = input("Nome do Pseudo-Herói: ")
+mestre = input("Nome do companheiro: ")
+vilao = input("Nome do vilão: ")
+arma_heroi = input("Arma usada pelo Pseudo-Herói: ")
+bioma = input("Localização geografica: ")
+nov_arm = input('Informe o nome de uma super arma: ')
+poder_nov = input ('Informe o poder da nova arma: ')
+print()
+print(f'''Após o vilão {vilao} torturar seu mestre {mestre}, o herói {heroi} sai em enfurecido. 
+porém, ele estava sem sua poderosa {arma_heroi}, então ele sai em busca de uma nova super arma {nov_arm}. O {heroi} atravessa a dureza do bioma
+de {bioma} atras do esconderijo de {vilao}, ao chegar no esconderijo {heroi} desafia {vilao} para 
+uma luta mortal. Ao decorrer da luta {vilao} se mostra superior a {heroi}..ate.. que.. BOOOM ele desperta sua nova super arma
+ela tem um poder {} mega, iper, super poderosa, poder esse de {} e ele se mostra superior ao vilão até que..
+{aparicao_supresa} chega e ajuda {heroi} porém quando a luta esta perto de acabar o aliado finalmente
+mostra sua verdadeira face e mata o herói pelas costas, ajudando o vilão. O {mestre} comprimenta seu colega agora no além.
+FIMKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK'''
