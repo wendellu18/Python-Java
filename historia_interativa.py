740a895dcf3327de6dbccb1207cb4dfe58f1a560
umjotta <heliojuniorsly@gmail.com>
2025-03-29 13:52:30 -0300

    Update HELIOALTERAÇÂO-historia_interativa.py

--- historia_interativa.py
+++ historia_interativa.py
@@ -1,8 +1,16 @@
-heroi = input("digite o nome de seu herói: ")
-mestre = input("digite o nome do mestre de seu herói: ")
-vilao = input("digite o nome de vilão: ")
-arma_heroi = input("informe a arma utilizada pelo seu heroi: ")
-bioma = input("digite o nome do bioma que a historia se passa: ")
-aparicao_supresa = input("digite o nome de um aliado supresa: ")
+heroi = input("Nome do Pseudo-Herói: ")
+mestre = input("Nome do companheiro: ")
+vilao = input("Nome do vilão: ")
+arma_heroi = input("Arma usada pelo Pseudo-Herói: ")
+bioma = input("Localização geografica: ")
+nov_arm = input('Informe o nome de uma super arma: ')
+poder_nov = input ('Informe o poder da nova arma: ')
 print()
-print(f"Após o vilão {vilao} matar seu mestre {mestre}, o herói {heroi} sai em busca de vingança. Com sua poderosa {arma_heroi}, {heroi} atravessa a dureza do bioma de {bioma} atras do esconderijo de {vilao}, ao chegar no esconderijo {heroi} desafia {vilao} para uma luta mortal. Ao decorrer da luta {vilao} se mostra superior a {heroi} ate que {aparicao_supresa} chega e ajuda {heroi} a vingar seu mestre {mestre}")
+print(f'''Após o vilão {vilao} torturar seu mestre {mestre}, o herói {heroi} sai em enfurecido. 
+porém, ele estava sem sua poderosa {arma_heroi}, então ele sai em busca de uma nova super arma {nov_arm}. O {heroi} atravessa a dureza do bioma
+de {bioma} atras do esconderijo de {vilao}, ao chegar no esconderijo {heroi} desafia {vilao} para 
+uma luta mortal. Ao decorrer da luta {vilao} se mostra superior a {heroi}..ate.. que.. BOOOM ele desperta sua nova super arma
+ela tem um poder {} mega, iper, super poderosa, poder esse de {} e ele se mostra superior ao vilão até que..
+{aparicao_supresa} chega e ajuda {heroi} porém quando a luta esta perto de acabar o aliado finalmente
+mostra sua verdadeira face e mata o herói pelas costas, ajudando o vilão. O {mestre} comprimenta seu colega agora no além.
+FIMKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK'''
